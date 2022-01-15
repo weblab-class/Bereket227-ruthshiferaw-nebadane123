e05c74bae55a0080c250658e2f422e5f01cd6103
Bereket227 <bereket.yeworku@gmail.com>
2022-01-14 22:16:39 -0500

    college_help_for_internationals

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
 import NavBar from "../modules/NavBar";
 
-const Home=() => {
+const Home=({ userId, handleLogin, handleLogout }) => {
     return(
         <div>
         <img src= "college1 2.png"/>
         <NavBar/>
+        {userId ? (
+        <GoogleLogout
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+          onFailure={(err) => console.log(err)}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={handleLogin}
+          onFailure={(err) => console.log(err)}
+        />
+      )}
         
 
         {/* <reviewsfrompaststudents/> */}
--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
+import Home from "./Home.js";
 
 import "../../utilities.css";
 import "./Skeleton.css";
@@ -10,6 +11,7 @@ const GOOGLE_CLIENT_ID = "32823633498-lv55i49eklchh7o1s494bdebe34stcsb.apps.goog
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
     <>
+    <Home/>
       {userId ? (
         <GoogleLogout
           clientId={GOOGLE_CLIENT_ID}
