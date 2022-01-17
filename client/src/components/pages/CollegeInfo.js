99acad45a6995b5d3426b8406ff0a602f13de798
Nebus <nebus12@mit.edu>
2022-01-16 23:02:50 -0500

    college_help_for_internationals

--- client/src/components/pages/CollegeInfo.js
+++ client/src/components/pages/CollegeInfo.js
@@ -8,18 +8,37 @@ import stanford from "./stanford.png";
 import MIT from "./MIT.png";
 import princeton from "./princeton.png";
 import yale from "./yale.png";
+import coo from "./coo.jpg";
 
 const CollegeInfo = () => {
   return (
     <>
       <section className="collegecontainer">
         <div className="listitle">Find colleges that fit you here</div>
-        <div className="list name">Columbia University</div>
-        <div className="list name">Harvard University</div>
-        <div className="list name">MIT University</div>
-        <div className="list name">Princeton University</div>
-        <div className="list name">Stanford University</div>
-        <div className="list name">Yale University</div>
+        <section>
+          <div className="list name">Columbia University</div>
+          <img className="img" scr={coo} />
+        </section>
+        <section>
+          <div className="list name">Harvard University</div>
+          <img className="img" scr={harvard} />
+        </section>
+        <section>
+          <div className="list name">MIT University</div>
+          <img className="img" scr={MIT} />
+        </section>
+        <section>
+          <div className="list name">Princeton University</div>
+          <img className="img" scr={princeton} />
+        </section>
+        <section>
+          <div className="list name">Stanford University</div>
+          <img className="img" scr={stanford} />
+        </section>
+        <section>
+          <div className="list name">Yale University</div>
+          <img className="img" scr={yale} />
+        </section>
       </section>
     </>
   );
