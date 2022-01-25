3d508b7552f37739108a7c1416046a311d96ed04
Nebus <nebus12@mit.edu>
2022-01-24 20:04:02 -0500

    college_help_for_internationals

--- client/src/components/pages/Vlogs.js
+++ client/src/components/pages/Vlogs.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import NavBar from "../modules/NavBar";
 
 const Vlogs = () => {
-  return <div>ergrg</div>;
+  return (
+    <div className="container">
+      <form className="form-group">
+        <input type="file" className="form-group" required />
+      </form>
+
+      <br></br>
+      <button type="Submit" className="btn">
+        UPLOAD
+      </button>
+      <br></br>
+      <h4> View PDF</h4>
+    </div>
+  );
 };
 
 export default Vlogs;
