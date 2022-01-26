da7e5b0915dd683ee43ef0f763efa86331c4e592
Nebus <nebus12@mit.edu>
2022-01-25 22:10:59 -0500

    college_help_for_internationals

--- client/src/components/pages/Vlogs.js
+++ client/src/components/pages/Vlogs.js
@@ -1,30 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "@reach/router";
 import NavBar from "../modules/NavBar";
-import { Viewer } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import { Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import "@react-pdf-viewer/core/lib/styles/index.css";
+// import { Viewer } from "@react-pdf-viewer/core";
+// import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+// import { Worker } from "@react-pdf-viewer/core";
+// import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import "./vlogs.css";
 
 export const Vlogs = () => {
   const [pdfFile, setPdfFile] = useState(null);
   const [pdfFileError, setPdfFileError] = useState("");
   const [viewPdf, setViewPdf] = useState(null);
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const [pageNum, setPageNum] = useState(1);
+  const [maxPageNum, setMaxPageNum] = useState(2);
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   const fileType = ["application/pdf"];
   const handlePdfFileChange = (e) => {
     let selectedFile = e.target.files[0];
     if (selectedFile) {
       if (selectedFile && fileType.includes(selectedFile.type)) {
-        let reader = new FileReader();
-        reader.readAsDataURL(selectedFile);
-        reader.onloadend = (e) => {
-          setViewPdf(e.target.result);
+        // let reader = new FileReader();
+        // reader.readAsDataURL(selectedFile);
+        // reader.onloadend = (e) => {
+        //   // console.log(e.target.result);
+        //   setViewPdf(e.target.result);
+        //   setPdfFileError("");
+        // };
+        selectedFile.arrayBuffer().then((pdfBuffer) => {
+          const pdfArray = new Uint8Array(pdfBuffer);
+          // console.log(pdfArray);
+          setViewPdf(pdfArray);
           setPdfFileError("");
-        };
+        });
       } else {
         setPdfFile(null);
         setPdfFileError("Please select valid pdf file");
@@ -34,6 +44,10 @@ export const Vlogs = () => {
     }
   };
   //form
+
+  const handleSuccess = (pdf) => {
+    setMaxPageNum(pdf.numPages);
+  };
 
   const handlePdfFileSubmit = (e) => {
     e.preventDefault();
@@ -78,12 +92,34 @@ export const Vlogs = () => {
         >
           {viewPdf && (
             <>
-              <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-                <Viewer fileUrl={viewPdf} plugins={[defaultLayoutPluginInstance]} />
-              </Worker>
+              <button
+                onClick={() => {
+                  pageNum > 1
+                    ? setPageNum((curNum) => {
+                        return curNum - 1;
+                      })
+                    : console.log("out of bounds");
+                }}
+              >
+                &lt;
+              </button>
+              <button
+                onClick={() => {
+                  pageNum < maxPageNum
+                    ? setPageNum((curNum) => {
+                        return curNum + 1;
+                      })
+                    : console.log("out of bounds");
+                }}
+              >
+                &gt;
+              </button>
+              <Document file={{ data: viewPdf }} onLoadSuccess={handleSuccess}>
+                <Page pageNumber={pageNum} />
+              </Document>
             </>
           )}
-          {!viewPdf && <>No pdf file selected</>}
+          <div className="select"> {!viewPdf && <>No pdf file selected</>}</div>
         </div>
       </div>
     </>
